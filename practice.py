d6d7401aa128ef189aff890487ac461488d5d096
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 22:24:04 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,18 @@
-print('hello')
-print('world')
-print()
+#Aliasing
+
+>>> a = [1,2,3]
+>>> b = a
+>>> b is a
+True
+
+>>> letters = ['a','b','c']
+>>> delete_head(letters)
+>>> letters
+['b','c']
+
+t1 = [1,2]
+t2 = t1.append(3)
+>>>t1
+[1,2,3]
+>>> t2
+None
